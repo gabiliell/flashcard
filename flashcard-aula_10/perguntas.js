a18e42045c67228731b6abda849055a68b734d41
gabiliell <161044753+gabiliell@users.noreply.github.com>
2024-11-01 10:45:38 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Espanha',
+    'A capital da Espanha é Madrid'
+)
+
+criaCartao(
+    'Programação',
+    'O que é Java?',
+    '-Linguagem de programação orientada a objetos, criado em 1995 criado'
+     
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz denada Inglês?',
+    'De forma formal "You are werlcome" já a informal mais utilizada seria "noi problems'
+)
